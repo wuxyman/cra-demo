b3df570090db75517b89d3f7162c3932dd68df6f
巫向阳 <wuxyboy@sina.com>
2020-11-19 01:14:17 +0800

    补配置

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,12 @@
 /* craco.config.js */
 const CracoLessPlugin = require('craco-less');
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
+// 产看打包进度
+const SimpleProgressWebpackPlugin = require( 'simple-progress-webpack-plugin' );
+// .gz 压缩
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
 
 const pathResolve = pathUrl => path.join(__dirname, pathUrl)
 
@@ -17,30 +23,45 @@ module.exports = {
       '@store': pathResolve('src/store'),
       '@utils': pathResolve('src/utils')
       // 此处是一个示例，实际可根据各自需求配置
-    }
+    },
+    plugins:[
+       // 打压缩包
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: new RegExp(
+            '\\.(' +
+            ['js', 'css'].join('|') +
+            ')$'
+        ),
+        threshold: 1024,
+        minRatio: 0.8
+      }),
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      new SimpleProgressWebpackPlugin(),
+    ],
   },
-    plugins: [
-        {
-          plugin: CracoLessPlugin,
-          options: {
-            lessLoaderOptions: {
-              lessOptions: {
-                modifyVars: { '@primary-color': '#1DA57A' },
-                javascriptEnabled: true,
-              },
+  plugins: [
+      {
+        plugin: CracoLessPlugin,
+        options: {
+          lessLoaderOptions: {
+            lessOptions: {
+              modifyVars: { '@primary-color': '#1DA57A' },
+              javascriptEnabled: true,
             },
           },
         },
-    ],
-    babel: {
-      plugins: [
-        ['import', { libraryName: 'antd', style: true }],
-        [
-          "@babel/plugin-proposal-decorators",
-          {
-            "legacy": true
-          }
-        ]
+      },
+  ],
+  babel: {
+    plugins: [
+      ['import', { libraryName: 'antd', style: true }],
+      [
+        "@babel/plugin-proposal-decorators",
+        {
+          "legacy": true
+        }
       ]
-    }
+    ]
+  }
 };
